47b5eedd16ab3209d2a796b366b4bb4b2497a66d
leah <leahcalicut@gmail.com>
2024-08-02 22:23:40 +0530

    dashboard latest

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,38 +1,102 @@
 import React from 'react'
 import './dashboard.css'
-import {FaEnvelope, FaUser,FaHome,FaCog ,FaBars, FaFileAlt} from 'react-icons/fa'
+import { FaEnvelope, FaUser, FaHome, FaCog, FaBars, FaFileAlt ,FaSearch} from 'react-icons/fa'
+
 
 const Dashboard = () => {
   return (
     <div className='dashboard-screen'>
-        <div className='app-bar'>
-            <h4>Dashboard</h4>
-            <div className='app-icons'>
-                <FaEnvelope className='icon'/>
-                <FaUser className='icon'/>
-
-            
-                
-            </div>
+      <div className='app-bar'>
+        <h4>Dashboard</h4>
+        <div className='app-icons'>
+          <FaEnvelope className='icon' />
+          <FaUser className='icon' />
         </div>
-        <div className='main-content'>
+      </div>
+      <div className='main-content'>
         <div className='side-menu'>
-            <img src='https://cdn-icons-png.freepik.com/512/7835/7835563.png' alt='logo' className='logo'></img>
-            <h4 className='name'>StoreZan</h4>
+          <img src='https://cdn-icons-png.freepik.com/512/7835/7835563.png' alt='logo' className='logo' />
+          <h4 className='name'>StoreZan</h4>
 
-            <div className='list'>
+          <div className='list'>
             <ul>
-                <li><FaHome className='menu-icon'/> Dashboard</li>
-                <li><FaBars className='menu-icon'/> Transactions</li>
-                <li><FaFileAlt className='menu-icon'/> Documents</li>
-                <li><FaCog className='menu-icon'/> Settings</li>
-                <li>Report</li>
-                <li>SignOut</li>
-            </ul></div>
-
-</div>
+              <li><FaHome className='menu-icon' /> Dashboard</li>
+              <li><FaBars className='menu-icon' /> Transactions</li>
+              <li><FaFileAlt className='menu-icon' /> Documents</li>
+              <li><FaCog className='menu-icon' /> Settings</li>
+              <li>Report</li>
+              <li>SignOut</li>
+            </ul>
+          </div>
         </div>
-        
+        <div className='cards'>
+          <div className='card card1'>Current Orders</div>
+          <div className='card card2'>Total Orders</div>
+          <div className='card card3'>Processed Orders</div>
+          <div className='card card4'>In Process Transactions <br />0</div>
+        </div>
+        <div className='table-container'>
+          <div className='recent'>Recently Added Products
+           
+           
+            <input type='text' placeholder='Search' className='search'></input>
+            <FaSearch className='search-icon'/>          
+          </div>
+          <div className='table'>
+            
+            <table>
+<thead>
+
+                <tr>
+              <th>#</th>
+              <th className='pic'>Picture</th>
+              <th className='sku'>Sku</th>
+              <th className='pname'>Name</th>
+              <th>Product Link </th></tr>
+              </thead><tbody>
+                <tr>
+                  <td>1</td>
+                  <td className='pic'><img src="" alt="product1" className="product-image" /></td>
+                  <td>12345</td>
+                  <td>Product Name 1</td>
+                  <td><button >View</button></td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td className='pic'><img src="" alt="product1" className="product-image" /></td>
+                  <td>12345</td>
+                  <td>Product Name 2</td>
+                  <td><button >View</button></td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td className='pic'><img src="" alt="product1" className="product-image" /></td>
+                  <td>12345</td>
+                  <td>Product Name 3</td>
+                  <td><button >View</button></td>
+                </tr>
+                
+                <tr>
+                  <td>5</td>
+                  <td className='pic'><img src="" alt="product1" className="product-image" /></td>
+                  <td>12345</td>
+                  <td>Product Name 5</td>
+                  <td><button >View</button></td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td className='pic'><img src="" alt="product1" className="product-image" /></td>
+                  <td>12345</td>
+                  <td>Product Name 6</td>
+                  <td><button >View</button></td>
+                </tr>
+                </tbody>
+                
+                
+                </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
